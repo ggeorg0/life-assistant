027343b5b4a7f3d0b44845dcb0814a1fc75540f1
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2023-12-11 23:48:40 +0300

    fix(plugins): move task to done instead of trash

--- tg-bot/plugins/randomtask_plugin.py
+++ tg-bot/plugins/randomtask_plugin.py
@@ -6,30 +6,36 @@ from notion import Notion
 class RandomCurrentTask(AbstractPlugin):
     
     # page_id of last chosen taks
-    _last_task: str | None
+    _last_task_id: str | None
+    _last_task_name: str
+    _last_created_in_done: str | None
     
     def __init__(self, name="RandomCurrentTask") -> None:
         super().__init__(name)
-        self._last_task = None
+        self._last_task_id = None
         self._notion = Notion()
     
     async def random_current_task(self, *args):
         tasks = await self._notion.current_tasks()
         task_id, task_title = choice(list(tasks.items()))
-        self._last_task = task_id
+        self._last_task_id = task_id
+        self._last_task_name = task_title
         return protect_for_html(task_title)
     
-    async def archive_last_task(self, *args):
-        if self._last_task:
-            await self._notion.archive_page(self._last_task)
-            return "The task is marked as completed!"
+    async def complete_last_task(self, *args):
+        if self._last_task_id:
+            self._last_created_in_done = await \
+                self._notion.move_to_done(self._last_task_id)
+            return f"The task \"{self._last_task_name}\" is marked as completed!"
         else:
             return "There are no last task!"
     
-    async def unarchive_last_task(self, *args):
-        if self._last_task:
-            await self._notion.unarchive_page(self._last_task)
-            return "The task is back. Oops!"
+    async def doagain_last_task(self, *args):
+        if self._last_task_id:
+            await self._notion.unarchive_page(self._last_task_id)
+            if self._last_created_in_done:
+                await self._notion.archive_page(self._last_created_in_done)
+            return "The task is back!"
         else:
             return "There are no last task!"
         
