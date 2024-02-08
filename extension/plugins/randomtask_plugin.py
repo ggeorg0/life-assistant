9ab79cb6f00e1cfd5fc509f852c03531560c0f3f
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2024-02-08 14:40:52 +0300

    feat: update RandomCurrentTask with new arc.

--- extension/plugins/randomtask_plugin.py
+++ extension/plugins/randomtask_plugin.py
@@ -1,51 +1,73 @@
+from collections.abc import Callable
+from datetime import datetime
 from random import choice
-from .interface import AbstractPlugin, ActionsCallbackTuple
+from typing import Any
+
+from extension import AbstractPlugin, ActionResult
+from extension.plgtyples import ActionT, CommandBindingsT, EventsScheduleT
 from tools import protect_for_html
 from notion import Notion
 
 class RandomCurrentTask(AbstractPlugin):
-    
+    __slots__ = (
+        "_last_task_id",
+        "_last_task_name",
+        "_last_created_in_done",
+        "_notion"
+    )
     # page_id of last chosen taks
     _last_task_id: str | None
     _last_task_name: str
     _last_created_in_done: str | None
-    
-    def __init__(self, name="RandomCurrentTask") -> None:
-        super().__init__(name)
+
+    def __init__(self) -> None:
+        super().__init__(name="RandomCurrentTask")
         self._last_task_id = None
         self._notion = Notion()
-    
-    async def random_current_task(self, *args):
+
+    async def random_current_task(self, *args) -> ActionResult:
         tasks = await self._notion.current_tasks()
         task_id, task_title = choice(list(tasks.items()))
         self._last_task_id = task_id
         self._last_task_name = task_title
-        return protect_for_html(task_title)
-    
-    async def complete_last_task(self, *args):
+        return ActionResult(
+            message=protect_for_html(task_title)
+        )
+
+    async def complete_last_task(self, *args) -> ActionResult:
         if self._last_task_id:
             self._last_created_in_done = await \
                 self._notion.move_to_done(self._last_task_id)
-            return f"The task \"{self._last_task_name}\" is marked as completed!"
+            message = f"The task \"{self._last_task_name}\" is marked as completed!"
         else:
-            return "There are no last task!"
-    
-    async def doagain_last_task(self, *args):
+            message = "There are no last task!"
+        return ActionResult(message=message)
+
+    async def doagain_last_task(self, *args) -> ActionResult:
         if self._last_task_id:
             await self._notion.unarchive_page(self._last_task_id)
             if self._last_created_in_done:
                 await self._notion.archive_page(self._last_created_in_done)
-            return "The task is back!"
+            message = "The task is back!"
         else:
-            return "There are no last task!"
-        
-    @property
-    def message_callabacks(self) -> tuple[ActionsCallbackTuple, ...]:
+            message = "There are no last task!"
+        return ActionResult(message=message)
+
+    def user_commands(self) -> CommandBindingsT:
+        return (
+            ("rtask", self.random_current_task),
+            ("task", self.random_current_task),
+            ("done", self.complete_last_task),
+            ("undone", self.doagain_last_task)
+        )
+
+    def daily_events(self) -> EventsScheduleT:
         return ()
-    
-    @property
-    def actions_callbacks(self) -> tuple[ActionsCallbackTuple, ...]:
+
+    def monthly_events(self) -> EventsScheduleT:
         return ()
-    
+
+    def disordered_events(self) -> EventsScheduleT:
+        return ()
 
 plg = RandomCurrentTask
