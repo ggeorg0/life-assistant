3f18e7a1ea22bfbd664f5b858117af0e4a35254d
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2024-02-28 13:35:19 +0300

    feat: update UniSchedule with new architecture
          Also
    	- add automatic evening schedule message
    	- add yesterday schedule command
    	- fix timeline in schedule message (update timezone)

--- extension/plugins/uni_schedule_plugin.py
+++ extension/plugins/uni_schedule_plugin.py
@@ -1,75 +1,141 @@
 from datetime import datetime, date, time, timedelta
-from typing import Callable, Sequence, Any
 import textwrap
 
-from plugins.interface import AbstractPlugin, ActionsCallbackTuple, MessageCallbackTuple
+from extension import AbstractPlugin, ActionResult
+from extension.exttypes import CommandBindingsT, EventsScheduleT
 from notion import Notion
-from tools import protect_for_html, time_from_args
-from config import DEFAULT_SCHEDULE_TIME
-
-
+from tools import protect_for_html, time_from_args, dt_from_time
+from config import (
+    TODAY_SCHED_TIME,
+    TOMMOROW_SCHED_TIME,
+    TIMEZONE,
+)
 
 class UniSchedule(AbstractPlugin):
-    _send_time: time
+    __slots__ = (
+        "_td_send_time",
+        "_notion",
+        "_tm_send_enabled",
+        "_tm_send_time"
+    )
+    _notion: Notion
+    _td_send_time: time
+    _tm_send_enabled: bool
+    _tm_send_time: time
 
-    def __init__(self, sending_time: time=DEFAULT_SCHEDULE_TIME) -> None:
+    # Note: Plugin methods should return `datetime` (not `time`),
+    #   but, for daily events date part is ignored, so
+    #   there is a temorary date wich will be combined with time
+
+    def __init__(self,
+                 td_schedule_send: time=TODAY_SCHED_TIME,
+                 tom_schedule_send: time=TOMMOROW_SCHED_TIME):
         super().__init__("UniSchedule")
         self._notion = Notion()
-        self._send_time = sending_time
+        self._td_send_time = td_schedule_send
+        self._tm_send_enabled = True
+        self._tm_send_time = tom_schedule_send
 
-    @property
-    def message_callabacks(self) -> tuple[MessageCallbackTuple, ...]:
-        send_datetime = datetime.combine(date.today(), self._send_time)
-        return ((send_datetime, self.today, 'daily'), )
-    
-    @property
-    def actions_callbacks(self) -> tuple[ActionsCallbackTuple, ...]:
-        # return ( (datetime.combine(datetime.today(), self._send_time.time()), 
-        #           self.some_action, 'daily'), )
+    def user_commands(self) -> CommandBindingsT:
+        return (
+            ("schedule", self.today),
+            ("yschedule", self.yesterday),
+            ("tschedule", self.tomorrow),
+            ("schedule_settime", self.set_sending_time),
+            ("tschedule_send", self.toggle_send_tomorrow),
+            ("tschedule_settime", self.set_tm_sending_time),
+        )
+
+    def daily_events(self) -> EventsScheduleT:
+        td_datetime = dt_from_time(self._td_send_time)
+        tmr_datetime = dt_from_time(self._tm_send_time)
+        return (
+            (td_datetime, self.today),
+            (tmr_datetime, self.tomorrow_autosend)
+        )
+
+    def monthly_events(self) -> EventsScheduleT:
         return ()
-    
+
+    def disordered_events(self) -> EventsScheduleT:
+        return ()
+
     async def form_schedule_message(self, day: date) -> str:
         daily_schedule = await self._notion.uni_daily_schedule(day)
         daily_schedule = sorted(daily_schedule)
-        message = ["Расписание:",
-                   "<pre>%-2s %-5s %-7s %-20s" % ("#", "нач.", "каб.", "предмет"),
+        message = ["%-2s %-5s %-7s %-20s" % ("#", "нач.", "каб.", "предмет"),
                    "-"*38]
         timeline_flag = True
         for num, pair_time, subj, _, auditory in daily_schedule:
-            ptime = time(*pair_time[1])
-            message_line = "%-2s %-2s:%-2s %-7s %-20s" % (num,
-                                                          ptime.hour, ptime.minute,
-                                                          auditory, 
-                                                          subj)
+            ptime = time(*pair_time[1], tzinfo=TIMEZONE)
+            message_line = "%-2s %-2s:%-2s %-7s %-20s" % (
+                num,
+                ptime.hour, ptime.minute,
+                auditory,
+                subj
+            )
             message_line = protect_for_html(message_line)
-            message_line = textwrap.wrap(message_line, width=38, 
+            message_line = textwrap.wrap(message_line, width=38,
                                          subsequent_indent=' '*17)
             # current timeline
-            if (datetime.now() < datetime.combine(date.today(), ptime)
-                    and timeline_flag):
+            if (datetime.now(TIMEZONE) < dt_from_time(ptime) and timeline_flag):
                 message_line.insert(0, protect_for_html('>' + '- '*18 + '<'))
                 timeline_flag = False
-
             message.append('\n'.join(message_line ) + '\n')
-        message[-1] = message[-1] + "</pre>"
-        return '\n'.join(message)
-        
-    async def today(self, *args) -> str:
-        return await self.form_schedule_message(date.today())
-    
-    async def tomorrow(self, *args) -> str:
-        return await self.form_schedule_message(date.today()
-                                                + timedelta(days=1))
-    async def yesterday(self, *args) -> str:
-        return await self.form_schedule_message(date.today()
-                                                - timedelta(days=1))
-    
-    async def set_sending_time(self, *args):
-        time_or_exc = time_from_args(args)
-        if type(time_or_exc) != time:
-            return time_or_exc
-        self._send_time = time_or_exc
-        return "New schedule send time:" + time_or_exc.strftime("%H:%M:%S")
+        return "<pre>" + '\n'.join(message) + "</pre>"
 
-    
-plg = UniSchedule
+    async def today(self, *args) -> ActionResult:
+        message=await self.form_schedule_message(date.today())
+        return ActionResult("Расписание на сегодня:\n" + message)
+
+    async def tomorrow(self, *args) -> ActionResult:
+        message = await self.form_schedule_message(date.today()
+                                                   + timedelta(days=1))
+        return ActionResult("Расписание на завтра:\n" + message)
+
+    async def yesterday(self, *args) -> ActionResult:
+        message=await self.form_schedule_message(date.today()
+                                                 - timedelta(days=1))
+        return ActionResult("Вчерашнее расписание:\n" + message)
+
+    async def tomorrow_autosend(self, *args) -> ActionResult:
+        if self._tm_send_enabled:
+            return await self.tomorrow(self, *args)
+        return ActionResult()
+
+    async def set_sending_time(self, *args) -> ActionResult:
+        time_or_exc = time_from_args(args)
+        if isinstance(time_or_exc, str):
+            return ActionResult(message=time_or_exc)
+        self._td_send_time = time_or_exc
+        return ActionResult(
+            message=("New schedule send time:" +
+                     time_or_exc.strftime("%H:%M:%S") + "\n" +
+                     "Please, reload daily events to apply this change")
+        )
+
+    async def toggle_send_tomorrow(self, *args: str) -> ActionResult:
+        if 0 < len(args) < 2:
+            match args[0].upper():
+                case "ON":
+                    self._tm_send_enabled = True
+                    return ActionResult(
+                        "enabled auto sending schedule for tommorow"
+                    )
+                case "OFF":
+                    self._tm_send_enabled = False
+                    return ActionResult(
+                        "diabled auto sending schedule for tommorow"
+                    )
+        return ActionResult("invalid arguments, use `on` or `off`")
+
+    async def set_tm_sending_time(self, *args) -> ActionResult:
+        time_or_exc = time_from_args(args)
+        if isinstance(time_or_exc, str):
+            return ActionResult(message=time_or_exc)
+        self._tm_send_time = time_or_exc
+        return ActionResult(
+            message=("New send time of schedule for tommorow :" +
+                     time_or_exc.strftime("%H:%M:%S") + "\n" +
+                     "Please, reload daily events to apply this change")
+        )
