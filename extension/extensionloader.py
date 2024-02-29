30427a4f9e39c90022e2cac6ddc2302ba545d2d3
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2024-02-29 17:02:45 +0300

    fix: check if plugin is enabled before action

--- extension/extensionloader.py
+++ extension/extensionloader.py
@@ -10,7 +10,7 @@ from telegram.ext import (
     CallbackContext
 )
 
-from extension import PluginManager, PluginLoader
+from extension import PluginManager, PluginLoader, AbstractPlugin
 from extension.exttypes import ActionT
 from config import TG_CHAT_ID, PLUGINS_DIR
 from tools import validate_user
@@ -35,24 +35,22 @@ class ExtensionLoader:
         self._plg_manager.set_plugins(plugins)
 
     def load_commands(self, app: Application):
-        for plg_name, cmd, action in self._plg_manager.user_commands():
-            callback = self.make_command_callback(action, plg_name)
+        for plg, cmd, action in self._plg_manager.user_commands():
+            callback = self.make_command_callback(action, plg)
             app.add_handler(CommandHandler(cmd, callback))
-            logging.info(f"[{plg_name}]: adding command /{cmd}")
-        for cmd, action in self._plg_manager.manage_commands():
-            callback = self.make_command_callback(action, 'Plugin Manager')
-            app.add_handler(CommandHandler(cmd, callback))
-            logging.info(f"Plugin Manager: adding command /{cmd}")
+            logging.info(f"[{plg.name}]: adding command /{cmd}")
 
-    def make_command_callback(self, action: ActionT, plg_name: str):
+    def make_command_callback(self, action: ActionT, plg: AbstractPlugin):
         @validate_user
         async def callback(update: Update,
                            context: ContextTypes.DEFAULT_TYPE):
+            if not plg.isenabled:
+                return
             command_args = context.args or ()
             try:
                 act_result = await action(*command_args)
             except Exception as e:
-                logging.error(f"[{plg_name}] user triggered action "
+                logging.error(f"[{plg.name}] user triggered action "
                               f"'{action.__name__}' FAILED")
                 print_exception(e)
                 return None
@@ -61,58 +59,60 @@ class ExtensionLoader:
                     update.effective_chat.id,
                     act_result.message
                 )
-            elif not (act_result.next_action is None
+            else:
+                logging.info(f"[{plg.name}] executed from user "
+                             "command: {action.__name__}")
+            if not (act_result.next_action is None
                       or act_result.next_datetime is None):
                 cb = self.create_event_callback(
                     act_result.next_action,
-                    plg_name
+                    plg
                 )
                 context.job_queue.run_once(
                     callback=cb,
                     when=act_result.next_datetime,
-                    name=plg_name,
+                    name=plg.name,
                     chat_id=update.effective_chat.id
                 )
-            else:
-                logging.info(f"[{plg_name}] executed from user "
-                             "command: {action.__name__}")
         return callback
 
     def load_daily_events(self, app: Application):
-        for plg_name, dt, action in self._plg_manager.daily_events():
-            callback = self.create_event_callback(action, plg_name)
+        for plg, dt, action in self._plg_manager.daily_events():
+            callback = self.create_event_callback(action, plg)
             app.job_queue.run_daily(
                 callback,
                 time=dt.time(),
-                name=plg_name,
+                name=plg.name,
                 chat_id=TG_CHAT_ID
             )
 
     def load_disordered_events(self, app: Application):
-        for plg_name, dt, action in self._plg_manager.disorder_events():
-            callback = self.create_event_callback(action, plg_name)
+        for plg, dt, action in self._plg_manager.disordered_events():
+            callback = self.create_event_callback(action, plg)
             app.job_queue.run_once(
                 callback,
                 when=dt,
-                name=plg_name,
+                name=plg.name,
                 chat_id=TG_CHAT_ID)
 
     def load_monthly_events(self, app: Application):
-        for plg_name, dt, action in self._plg_manager.monthly_events():
-            callback = self.create_event_callback(action, plg_name)
+        for plg, dt, action in self._plg_manager.monthly_events():
+            callback = self.create_event_callback(action, plg)
             app.job_queue.run_monthly(
                 callback,
                 when=dt.time(),
                 day=dt.day,
-                name=plg_name,
+                name=plg.name,
                 chat_id=TG_CHAT_ID)
 
-    def create_event_callback(self, action: ActionT, plg_name: str):
+    def create_event_callback(self, action: ActionT, plg: AbstractPlugin):
         async def callback(context: CallbackContext):
+            if not plg.isenabled:
+                return
             try:
                 act_result = await action()
             except Exception as e:
-                logging.error(f"[{plg_name}] triggered action "
+                logging.error(f"[{plg.name}] triggered action "
                               f"'{action.__name__}' FAILED")
                 print_exception(e)
                 return None
@@ -121,20 +121,20 @@ class ExtensionLoader:
                     context.job.chat_id,
                     act_result.message
                 )
-            elif not (act_result.next_action is None
+            else:
+                logging.info(f"[{plg.name}] executed event "
+                             "action: {action.__name__}")
+            if not (act_result.next_action is None
                       or act_result.next_datetime is None):
                 cb = self.create_event_callback(
                     act_result.next_action,
-                    plg_name
+                    plg
                 )
                 context.job_queue.run_once(
                     callback=cb,
                     when=act_result.next_datetime,
-                    name=plg_name,
+                    name=plg.name,
                     chat_id=context.job.chat_id
                 )
-            else:
-                logging.info(f"[{plg_name}] executed event "
-                             "action: {action.__name__}")
         return callback
 
