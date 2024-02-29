ae410f72d5f326fb9f4944d18d66068eb28b3998
ggeorge <89857543+ggeorg0@users.noreply.github.com>
2024-02-29 16:58:21 +0300

    feat: PlgManager is subclass of AbstractPlugin now

--- extension/plgmanager.py
+++ extension/plgmanager.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from extension import AbstractPlugin, ActionResult
 
 
-class PluginManager:
+class PluginManager(AbstractPlugin):
     # Singletone class
     _ids = count(0)
     _loaded_plugins: dict[str, AbstractPlugin]
@@ -15,19 +15,22 @@ class PluginManager:
         self.id = next(self._ids)
         if self.id > 1:
             raise RuntimeError("There are additional PluginManager instance")
+        super().__init__('PluginManager')
         self.set_plugins(plugins)
 
-    def user_commands(self) -> Iterable[tuple[str, str, Callable]]:
+    def user_commands(self) -> Iterable[tuple[AbstractPlugin, str, Callable]]:
         logging.info("Plugin Manager: loading user commands")
-        for plg_name, p in self._loaded_plugins.items():
+        for p in self._loaded_plugins.values():
             if p.isenabled:
                 for command, action in p.user_commands():
-                    yield (plg_name, command, action)
+                    yield (p, command, action)
             else:
-                logging.info(f"Plugin Manager: skipping [{plg_name}]"
+                logging.info(f"Plugin Manager: skipping [{p.name}]"
                              " (not enabled)")
+        for command, action in self._manage_commands():
+            yield (self, command, action)
 
-    def manage_commands(self) -> Iterable[tuple[str, Callable]]:
+    def _manage_commands(self) -> Iterable[tuple[str, Callable]]:
         return (
             ("enable", self.enable_plugin),
             ("disable", self.disable_plugin),
@@ -35,34 +38,34 @@ class PluginManager:
             ("pl", self.list_plugins)
         )
 
-    def daily_events(self) -> Iterable[tuple[str, datetime, Callable]]:
+    def daily_events(self) -> Iterable[tuple[AbstractPlugin, datetime, Callable]]:
         logging.info("Plugin Manager: loading daily events")
-        for plg_name, p in self._loaded_plugins.items():
+        for p in self._loaded_plugins.values():
             if p.isenabled:
                 for dt, action in p.daily_events():
-                    yield (plg_name, dt, action)
+                    yield (p, dt, action)
             else:
-                logging.info(f"Plugin Manager: skipping [{plg_name}]"
+                logging.info(f"Plugin Manager: skipping [{p.name}]"
                              " (not enabled)")
 
-    def monthly_events(self) -> Iterable[tuple[str, datetime, Callable]]:
+    def monthly_events(self) -> Iterable[tuple[AbstractPlugin, datetime, Callable]]:
         logging.info("Plugin Manager: loading monthly events")
-        for plg_name, p in self._loaded_plugins.items():
+        for p in self._loaded_plugins.values():
             if p.isenabled:
                 for dt, action in p.monthly_events():
-                    yield (plg_name, dt, action)
+                    yield (p, dt, action)
             else:
-                logging.info(f"Plugin Manager: skipping [{plg_name}]"
+                logging.info(f"Plugin Manager: skipping [{p.name}]"
                              " (not enabled)")
 
-    def disorder_events(self) -> Iterable[tuple[str, datetime, Callable]]:
+    def disordered_events(self) -> Iterable[tuple[AbstractPlugin, datetime, Callable]]:
         logging.info("Plugin Manager: loading disordered events")
-        for plg_name, p in self._loaded_plugins.items():
+        for p in self._loaded_plugins.values():
             if p.isenabled:
                 for dt, action in p.disordered_events():
-                    yield (plg_name, dt, action)
+                    yield (p, dt, action)
             else:
-                logging.info(f"Plugin Manager: skipping [{plg_name}]"
+                logging.info(f"Plugin Manager: skipping [{p.name}]"
                              " (not enabled)")
 
     def set_plugins(self, plugins: Iterable[AbstractPlugin]):
@@ -73,23 +76,32 @@ class PluginManager:
 
     async def enable_plugin(self, *args):
         """User command for enabling plugin by name (args)"""
-        return self._switch_plugin(self._enable_plugin, "enabled now", *args)
+        return self._switch_plugin(self._enable_plugin, *args)
 
     async def disable_plugin(self, *args):
         """User command for disabling plugin by name (args)"""
-        return self._switch_plugin(self._diable_plugin, "disabled now", *args)
+        return self._switch_plugin(self._diable_plugin, *args)
 
-    def _switch_plugin(self, switcher, success_message: str, *args):
+    def _switch_plugin(self, switcher: Callable, *args):
         """Wrapper for _enable and _disable methods"""
         try:
             plugin_name = " ".join(args)
-            switcher(plugin_name)
-            return ActionResult(f"[{plugin_name}] is {success_message}")
+            return switcher(plugin_name)
         except (IndexError, KeyError):
                 return ActionResult(
                     "Error: you must specify correct plugin name\n"
                     "Hint: use /plugins to see loaded plugins"
                 )
+
+    def _enable_plugin(self, name) -> ActionResult:
+        self._get_plugin(name).enable()
+        return ActionResult(f"[{name}] is enabled now")
+
+    def _diable_plugin(self, name):
+        if name == self.name:
+            return ActionResult("You cannot turn off PluginManager")
+        self._get_plugin(name).disable()
+        return ActionResult(f"[{name}] is disabled now")
 
     async def list_plugins(self) -> ActionResult:
         """User command for listing loaded plugins"""
@@ -102,9 +114,6 @@ class PluginManager:
         message.append('</pre>')
         return ActionResult('\n'.join(message))
 
-    def _enable_plugin(self, name):
-        self._get_plugin(name).enable()
-
-    def _diable_plugin(self, name):
-        self._get_plugin(name).disable()
+    def disable(self):
+        logging.error("It is not possible to disable the PluginManager")
 
